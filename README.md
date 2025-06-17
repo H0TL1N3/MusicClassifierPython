1d38b269f3adb317f1842c2dccdc93068e1a175b
H0TL1N3 <ilja2002k@gmail.com>
2025-06-17 11:30:56 +0300

    Update README.md, playlist functionality, layout

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Music Classifier GUI program for ValTehPamati course
 
 ## Steps to compile
-1. Install Python 3.11 on your system as it will be used for this project. If you're on Windows, you can get a binary release, version 3.11.9, from [this link](https://www.python.org/downloads/windows/).
-2. Set up a virtual environment using either the command line or your IDE of choice. Here's a [guide on how to do that in PyCharm](https://www.jetbrains.com/help/pycharm/creating-virtual-environment.html#python_create_virtual_env).
-3. Install all required libraries from the requirements.txt - once again, PyCharm should take care of that and notify you about this need.
+1. Install Python 3.11 on your system as it will be used for this project. 
+If you're on Windows, you can get a binary release, version 3.11.9, from [this link](https://www.python.org/downloads/windows/).
+2. Set up a virtual environment using either the command line or your IDE of choice. 
+Here's a [guide on how to do that in PyCharm](https://www.jetbrains.com/help/pycharm/creating-virtual-environment.html#python_create_virtual_env).
+3. Install all required libraries from the requirements.txt - once again, 
+PyCharm should take care of that and notify you about installing them.
 4. You can now compile the project and play around with it.
+
+## Notes about the project
+1. Playlists are not saved to storage, which is the intended behavior. 
+We keep them in memory during runtime.
+2. Accepted notation for keywords and attributes when using `ttkbootstrap` (in this project)
+is not through Constants, but through strings -
+PyCharm tends to give warnings if Constants are used. 
+   1. Please note that `bootstyle` will probably give a warning no matter what approach you use.
+   2. Here is some info about [keyword usage in ttkbootstrap](https://ttkbootstrap.readthedocs.io/en/latest/gettingstarted/tutorial/#keyword-usage).
 
 ## IMPORTANT: Please Note that the project is not yet complete.
 ### TODO List:
 1. Add models (BERT, NB)...
 2. Implement functions that will classify the text.
-3. Implement the player itself.
+3. Implement playback of files.
+### DONE List:
+1. Basic layout for the app.
+2. Playlist functionality.
+3. File addition.
+4. Audio transcription using OpenAI Whisper.
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ## GUI
 from tkinter import filedialog as fd
 import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
 from ttkbootstrap.icons import Emoji
 from ttkbootstrap.scrolled import ScrolledText
 ## OpenAI Whisper
@@ -12,22 +11,51 @@ import threading
 class MediaPlayer(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack(fill=BOTH, expand=YES)
+        self.pack(fill="both", expand=True)
         # Internal state
         self.hdr_var = ttk.StringVar()
         self.elapsed_var = ttk.DoubleVar(value=0)
         self.remain_var = ttk.DoubleVar(value=190)
-        # TODO: Use this in display.
+        # Playlist and chosen track state
         self.playlists = {
             "BERT": [],
             "NaiveBayes": []
         }
         self.current_tab = "BERT"
+        self.notebook = None
+        self.playlist_frames = {}
+        self.current_track_index = None
+        # Scrolled Text state
+        self.transcribedText = None
+        # Progress bar state
+        self.elapse = None
+        self.scale = None
+        self.remain = None
+        # Layout
+        self.main = None
+        self.top = None
+        self.left = None
+        self.right = None
+        self.bottom = None
         # Creating GUI elements
+        self.create_layout()
         self.create_header()
         self.create_text_window()
+        self.create_playlists()
         self.create_progress_meter()
         self.create_buttonbox()
+
+    def create_layout(self):
+        self.main = ttk.Frame(self)
+        self.main.pack(fill="both", expand=True)
+        self.top = ttk.Frame(self.main)
+        self.top.pack(side="top", fill="both", expand=True)
+        self.left = ttk.Frame(self.top)
+        self.left.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+        self.right = ttk.Frame(self.top)
+        self.right.pack(side="right", fill="y", padx=5, pady=5)
+        self.bottom = ttk.Frame(self.main)
+        self.bottom.pack(side="bottom", fill="both", padx=5, pady=5)
 
     def create_header(self):
         """The application header to display user messages"""
@@ -35,39 +63,58 @@ class MediaPlayer(ttk.Frame):
         lbl = ttk.Label(
             master=self,
             textvariable=self.hdr_var,
-            bootstyle=(LIGHT, INVERSE),
+            bootstyle="light-inverse",
             padding=10
         )
-        lbl.pack(fill=X, expand=YES)
+        lbl.pack(fill="x", expand=True)
 
     def create_text_window(self):
         """Create frame to contain scrolled text"""
-        # Store in self to access later in button
-        self.st = ScrolledText(self, padding=5, height=10, autohide=True)
-        self.st.pack(fill=BOTH, expand=YES)
+        # Store in self to access later
+        self.transcribedText = ScrolledText(self.left, padding=5, height=10, autohide=True)
+        self.transcribedText.pack(fill="both", expand=True)
+
+    def create_playlists(self):
+        """Create frame to contain playlists"""
+        # Store in self to access later
+        self.notebook = ttk.Notebook(self.right)
+        self.notebook.pack(fill="both", expand=True)
+        self.playlist_frames = {}
+
+        for tab_name in self.playlists.keys():
+            frame = ttk.Frame(self.notebook)
+            self.notebook.add(frame, text=tab_name)
+            # Inner frame for entries in playlist
+            content_frame = ttk.Frame(frame)
+            content_frame.pack(fill="both", expand=True, padx=5, pady=5)
+            self.playlist_frames[tab_name] = content_frame
+            self.render_playlist(tab_name)
+
+        # Handle tab change
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_change)
 
     def create_progress_meter(self):
-        """Create frame with progress meter with lables"""
-        container = ttk.Frame(self)
-        container.pack(fill=X, expand=YES, pady=10)
+        """Create frame with progress meter with labels"""
+        container = ttk.Frame(self.bottom)
+        container.pack(fill="x", expand=True, pady=10)
 
         self.elapse = ttk.Label(container, text='00:00')
-        self.elapse.pack(side=LEFT, padx=10)
+        self.elapse.pack(side="left", padx=10)
 
         self.scale = ttk.Scale(
             master=container,
             command=self.on_progress,
-            bootstyle=SECONDARY
+            bootstyle="secondary"
         )
-        self.scale.pack(side=LEFT, fill=X, expand=YES)
+        self.scale.pack(side="left", fill="x", expand=True)
 
         self.remain = ttk.Label(container, text='03:10')
-        self.remain.pack(side=LEFT, fill=X, padx=10)
+        self.remain.pack(side="left", fill="x", padx=10)
 
     def create_buttonbox(self):
         """Create buttonbox with media controls"""
-        container = ttk.Frame(self)
-        container.pack(fill=X, expand=YES)
+        container = ttk.Frame(self.bottom)
+        container.pack(fill="x", expand=True)
         ttk.Style().configure('TButton', font="-size 14")
 
         rev_btn = ttk.Button(
@@ -75,44 +122,44 @@ class MediaPlayer(ttk.Frame):
             text=Emoji.get('black left-pointing double triangle with vertical bar'),
             padding=10,
         )
-        rev_btn.pack(side=LEFT, fill=X, expand=YES)
+        rev_btn.pack(side="left", fill="x", expand=True)
 
         play_btn = ttk.Button(
             master=container,
             text=Emoji.get('black right-pointing triangle'),
             padding=10,
         )
-        play_btn.pack(side=LEFT, fill=X, expand=YES)
+        play_btn.pack(side="left", fill="x", expand=True)
 
         fwd_btn = ttk.Button(
             master=container,
             text=Emoji.get('black right-pointing double triangle with vertical bar'),
             padding=10,
         )
-        fwd_btn.pack(side=LEFT, fill=X, expand=YES)
+        fwd_btn.pack(side="left", fill="x", expand=True)
 
         pause_btn = ttk.Button(
             master=container,
             text=Emoji.get('double vertical bar'),
             padding=10,
         )
-        pause_btn.pack(side=LEFT, fill=X, expand=YES)
+        pause_btn.pack(side="left", fill="x", expand=True)
 
         stop_btn = ttk.Button(
             master=container,
             text=Emoji.get('black square for stop'),
             padding=10,
         )
-        stop_btn.pack(side=LEFT, fill=X, expand=YES)
+        stop_btn.pack(side="left", fill="x", expand=True)
 
         stop_btn = ttk.Button(
             master=container,
-            text=Emoji.get('open file folder'),
-            bootstyle=SECONDARY,
-            command=lambda: self.file_dialog(self.st),
+            text=Emoji.get("open file folder"),
+            bootstyle="secondary",
+            command=lambda: self.file_dialog(),
             padding=10
         )
-        stop_btn.pack(side=LEFT, fill=X, expand=YES)
+        stop_btn.pack(side="left", fill="x", expand=True)
 
     def on_progress(self, val: float):
         """Update progress labels when the scale is updated."""
@@ -134,31 +181,71 @@ class MediaPlayer(ttk.Frame):
         self.elapse.configure(text=f'{elapse_min:02d}:{elapse_sec:02d}')
         self.remain.configure(text=f'{remain_min:02d}:{remain_sec:02d}')
 
-    def file_dialog(self, text_display):
+    ###
+    ### Helper functions
+    ###
+    def on_tab_change(self, event):
+        selected_index = self.notebook.index("current")
+        self.current_tab = list(self.playlists.keys())[selected_index]
+        self.current_track_index = None
+        self.transcribedText.delete("1.0", "end")
+
+    def render_playlist(self, playlist_name):
+        # Get
+        frame = self.playlist_frames.get(playlist_name)
+        if not frame:
+            return
+        # Clear
+        for widget in frame.winfo_children():
+            widget.destroy()
+        # Fill
+        for index, entry in enumerate(self.playlists[playlist_name]):
+            filename = entry["path"].split("/")[-1]
+            btn = ttk.Button(
+                frame,
+                text=filename,
+                bootstyle="secondary",
+                command=lambda idx=index, name=playlist_name: self.select_track(name, idx)
+            )
+            btn.pack(fill="x", pady=2)
+
+    def select_track(self, playlist_name, index):
+        entry = self.playlists[playlist_name][index]
+        # Redundant change of playlist, but may be useful in some scenarios
+        self.current_tab = playlist_name
+        self.current_track_index = index
+
+        self.transcribedText.delete("1.0", "end")
+        self.transcribedText.insert("end", entry["text"])
+
+    def file_dialog(self):
         file_path = fd.askopenfilename(filetypes=[("Audio Files", "*.mp3 *.wav *.flac *.ogg *.m4a")])
-        threading.Thread(target=self.transcribe_audio, args=(file_path, text_display, self.current_tab)).start()
+        self.add_track(file_path, self.current_tab)
+        threading.Thread(target=self.transcribe_audio, args=(file_path, self.current_tab)).start()
+
+    def add_track(self, file_path, tab):
         entry = {
             "path": file_path,
             "text": "In Progress"
         }
-        self.playlists[self.current_tab].append(entry)
+        self.playlists[tab].append(entry)
+        self.render_playlist(tab)
 
-    # TODO: currently operates directly on the text_display. Make it simply transcribe audio into the entry.
-    def transcribe_audio(self, file_path, text_display, current_tab):
-        text_display.delete("1.0", END)
-        text_display.insert(END, "Transcribing... Please wait.\n")
+    def transcribe_audio(self, file_path, tab):
         try:
             result = model.transcribe(file_path)
-            text_display.delete("1.0", END)
-            text_display.insert(END, result["text"])
-            for entry in self.playlists[current_tab]:
+            print("done")
+            for idx, entry in enumerate(self.playlists[tab]):
                 if entry["path"] == file_path:
-                    entry["text"] = result["text"]
-                    print(entry)
-                    return
+                    self.playlists[tab][idx]["text"] = result["text"]
+                    # TODO: Test later that this callback works correctly.
+                    # Callback to rerender lyrics
+                    if idx == self.current_track_index:
+                        self.select_track(tab, idx)
         except Exception as e:
-            text_display.delete("1.0", END)
-            text_display.insert(END, f"Error: {e}")
+            for entry in self.playlists[self.current_tab]:
+                if entry["path"] == file_path:
+                    entry["text"] = f"Error during transcription: {e}"
 
 if __name__ == '__main__':
     root = ttk.Window()
